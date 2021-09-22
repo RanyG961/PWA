8641a19c7b39a830f4a15486dd73750a4731d2f1
Rany G <ranywwe@hotmail.com>
2021-09-22 09:21:17 +0200

    Adding the project structure

---
+++ wintter/src/main/java/pwa/projet/wintter/WintterApplication.java
@@ -0,0 +1,13 @@
+package pwa.projet.wintter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WintterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WintterApplication.class, args);
+	}
+
+}
